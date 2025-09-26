8faea675f7c0c041759d82281f00dfe92365287d
toto-inu <oztodog@gmail.com>
2025-09-26 20:44:21 +0900

    add postrequest.ts

--- documents/001_howtostart.md
+++ documents/001_howtostart.md
@@ -3,22 +3,23 @@
 
 
 - [x] all-in-oneとは？
-- [ ] TypeScriptが直で実行できる？
+- [x] TypeScriptが直で実行できる？
 - [ ] 実行速度が速い？
 - [ ] denoとbunの比較
 - [ ] bunを実務利用する上での懸念
 
 ハンズオンしたいところ
-- [ ] bun.serveでサーバーを立ち上げる
-- [ ] 
+- [x] bun.serveでサーバーを立ち上げる
+- [ ] s3へのファイルアップロードと、localファイル作成のコード差分
+- [ ] Postgresへの接続
 
 
 ## all-in-oneとは？
 
 - postgresとかs3への接続が組み込みのAPIでできる
-- s3へのファイルアップロードと、localファイル作成のコード差分
 
 ## Tips
 
 - bunはyarnやnpmにも代替する
+- bun.serveはデフォルトではホットロードされない
 - 
--- working/001_typescript/index.ts
+++ working/001_typescript/index.ts
@@ -1,4 +1,31 @@
+type PostObject = {
+  id: string;
+  title: string;
+  content?: string;
+}
 
-const hoge:string = "totoinu";
+const server = Bun.serve({
+  port: 3000,
+  routes: {
+    "/api/status": new Response("OK"),
 
-console.log(`Hello via Bun! ${hoge}`);
+    // Dynamic routes
+    "/users/:id": req => {
+      return new Response(`Hello User ${req.params.id}!`);
+    },
+
+    // Per-HTTP method handlers
+    "/api/posts": {
+      GET: () => new Response("List posts"),
+      POST: async req => {
+        const body = await req.json() as PostObject;
+        return Response.json({ created: true, ...body });
+      },
+    },
+  },
+  fetch(req) {
+    return new Response("Bun!");
+  },
+});
+
+console.log(`Listening on http://localhost:${server.port} ...`);
